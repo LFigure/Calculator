3337a84cf76772b63cc99eeb99846be109b468f1
Sami <saminassif01@gmail.com>
2024-02-06 22:47:15 -0500

    Added the base of the functions of the calculator, haven't added any logic in JS yet, customized what I have done with some css.

---
+++ script.js
@@ -0,0 +1,42 @@
+//Basic operators
+function addition(){
+
+}
+
+function subtraction(){
+
+}
+
+function multiplication(){
+
+}
+
+function divide(){
+
+}
+
+
+
+//Special operators
+function squareRoot(){
+
+}
+
+function power(){
+
+}
+
+function pi(){
+
+}
+
+function modulous(){
+
+}
+
+
+//Equalization
+function equals(){
+    
+}
+
